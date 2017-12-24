a907c12f99d719a248674a15710ae5b213b6d5fd
Maurice-Betzel <maurice@betzel.net>
2017-12-24 14:42:41 +0100

    Working windows x86_64 build

--- libpostal/cppbuild.sh
+++ libpostal/cppbuild.sh
@@ -7,27 +7,43 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
+echo "downloading libpostal"
+
+LIBPOSTAL_VERSION=master
+download https://github.com/openvenues/libpostal/archive/$LIBPOSTAL_VERSION.tar.gz libpostal-$LIBPOSTAL_VERSION.tar.gz
+
+echo "unpacking libpostal"
+
 mkdir -p $PLATFORM
-
 cd $PLATFORM
+tar -xzvf ../libpostal-$LIBPOSTAL_VERSION.tar.gz
+cd libpostal-$LIBPOSTAL_VERSION
 
-mkdir -p libpostal
-
-cd libpostal
 
 case $PLATFORM in
     windows-x86_64)
-        unzip ../../../libpostal_binary.zip
-        mkdir -p ../bin
-        mkdir -p ../lib
-        mkdir -p ../include
-        cp *.dll ../bin
-        cp *.lib ../lib
-        cp *.h ../include
+    echo "Starting windows-x86_64 build"
+    cp -rf windows/* ./
+    cwd=$(pwd)
+    bash -lc "cd $cwd && autoreconf -fi --warning=no-portability"
+    #./configure --datadir=/c
+    ./configure --disable-data-download
+    make -j4
+    make install
+    mkdir -p ../bin
+    mkdir -p ../lib
+    mkdir -p ../include
+
+    lib.exe /def:libpostal.def /out:libpostal.lib /machine:x64
+
+    cp src/.libs/libpostal-1.dll ../bin
+    cp libpostal.lib ../lib
+    cp src/libpostal.h ../include
         ;;
     *)
         echo "Error: Platform \"$PLATFORM\" is not supported"
-        ;;
+;;
+
 esac
 
 cd ../..
