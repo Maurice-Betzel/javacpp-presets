9465b0422e201cc355a04407b5329682a6922a5e
Maurice-Betzel <maurice@betzel.net>
2017-12-25 11:47:26 +0100

    Working windows x86_64 build

--- libpostal/README.md
+++ libpostal/README.md
@@ -19,3 +19,18 @@ Java API documentation is available here:
 
 Sample Usage
 ------------
+```java
+boolean setup1 = libpostal.libpostal_setup();
+BytePointer trainingDataPath = new BytePointer("C:/libpostal", "UTF-8");
+boolean setup3 = libpostal.libpostal_setup_datadir(trainingDataPath);
+boolean setup2 = libpostal.libpostal_setup_language_classifier();
+libpostal.libpostal_normalize_options_t options = libpostal.libpostal_get_default_options();
+SizeTPointer sizeTPointer = new SizeTPointer(0);
+BytePointer address = new BytePointer("Quatre vingt douze Ave des Champs-Elysees", "UTF-8");
+PointerPointer pointerPointer = libpostal.libpostal_expand_address(address, options, sizeTPointer);
+long t_size = sizeTPointer.get(0);
+for (long i = 0; i < t_size; i++) {
+    System.out.println(pointerPointer.getString(i));
+}
+libpostal.libpostal_teardown();
+libpostal.libpostal_teardown_language_classifier();
--- libpostal/cppbuild.sh
+++ libpostal/cppbuild.sh
@@ -7,35 +7,41 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
-echo "downloading libpostal"
-
 LIBPOSTAL_VERSION=master
 download https://github.com/openvenues/libpostal/archive/$LIBPOSTAL_VERSION.tar.gz libpostal-$LIBPOSTAL_VERSION.tar.gz
-
-echo "unpacking libpostal"
 
 mkdir -p $PLATFORM
 cd $PLATFORM
 tar -xzvf ../libpostal-$LIBPOSTAL_VERSION.tar.gz
 cd libpostal-$LIBPOSTAL_VERSION
 
-
 case $PLATFORM in
+    linux-x86_64)
+    ./bootstrap.sh
+    #./configure --datadir=[...some dir with a few GB of space...]
+    ./configure --disable-data-download
+    make -j4
+    sudo make install
+        ;;
+    macosx-*)
+    ./bootstrap.sh
+    #./configure --datadir=[...some dir with a few GB of space...]
+    ./configure --disable-data-download
+    make -j4
+    sudo make install
+    ;;
     windows-x86_64)
-    echo "Starting windows-x86_64 build"
     cp -rf windows/* ./
     cwd=$(pwd)
     bash -lc "cd $cwd && autoreconf -fi --warning=no-portability"
-    #./configure --datadir=/c
+    #./configure --datadir=/c [...some dir with a few GB of space...]
     ./configure --disable-data-download
     make -j4
     make install
+    lib.exe /def:libpostal.def /out:libpostal.lib /machine:x64
     mkdir -p ../bin
     mkdir -p ../lib
     mkdir -p ../include
-
-    lib.exe /def:libpostal.def /out:libpostal.lib /machine:x64
-
     cp src/.libs/libpostal-1.dll ../bin
     cp libpostal.lib ../lib
     cp src/libpostal.h ../include
